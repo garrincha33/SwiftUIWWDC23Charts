0cc26d1ee938a0228aca7778161452cdba7319ff
Richy <garrincha33@users.noreply.github.com>
2023-07-04 14:20:06 +0100

    added picker

---
+++ SwiftUIWWDCCharts/View/Home.swift
@@ -0,0 +1,28 @@
+//
+//  Home.swift
+//  SwiftUIWWDCCharts
+//
+//  Created by Richard Price on 04/07/2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State private var graphType: GraphType = .donut
+    
+    var body: some View {
+        
+        VStack {
+            ///Segmented Picker
+            Picker("", selection: $graphType) {
+                ForEach(GraphType.allCases, id: \.rawValue) { type in
+                    Text(type.rawValue).tag(type)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
