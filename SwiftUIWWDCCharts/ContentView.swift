5edc6f194253b3f432e27431aedf917d104e7c84
Richy <garrincha33@users.noreply.github.com>
2023-07-04 13:45:03 +0100

    Initial Commit

---
+++ SwiftUIWWDCCharts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIWWDCCharts
+//
+//  Created by Richard Price on 04/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIWWDCCharts/SwiftUIWWDCChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWWDCChartsApp.swift
+//  SwiftUIWWDCCharts
+//
+//  Created by Richard Price on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWWDCChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
